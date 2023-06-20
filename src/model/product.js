e89dc080c503ebda7f219443691c447c9defd775
Muhammad Ali <ali.muhammad751935@gmail.com>
2023-06-20 16:19:16 +0500

    updates the schema

--- src/model/product.js
+++ src/model/product.js
@@ -1,21 +1,28 @@
 const mongoose = require("mongoose");
-const mongoosePaginate = require('mongoose-paginate-v2');
+const mongoosePaginate = require("mongoose-paginate-v2");
 
-const productSchema = new mongoose.Schema({
-  title: {
-    required: true,
-    type: String,
+const productSchema = new mongoose.Schema(
+  {
+    title: {
+      required: true,
+      type: String,
+    },
+    moq: {
+      required: true,
+      type: Number,
+    },
+    image: { type: String, required: true },
+    rating: { type: Number, min: 0, max: 5 },
+    min_price: { required: true, type: Number },
+    max_price: { type: Number },
+    unit: { required: true, type: String },
+    product_certificate: { required: true, type: [String] },
+    supplier_certificate: { required: true, type: [String] },
+    country: { required: true, type: [String] },
+    stock_in_usa: { type: Boolean },
   },
-  moq: {
-    required: true,
-    type: Number,
-  },
-  lowerPriceRange: { required: true, type: Number },
-  upperPriceRange: { required: false, type: Number },
-  type: { required: true, type: String },
-  category: { required: true, type: String }
-
-}, { timestamps: true });
+  { timestamps: true }
+);
 
 productSchema.plugin(mongoosePaginate);
 
