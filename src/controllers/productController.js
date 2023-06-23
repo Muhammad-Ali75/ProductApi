3107d81616a4e99d537e9e3d88a81e10a3747a08
Muhammad Ali <ali.muhammad751935@gmail.com>
2023-06-23 15:12:00 +0500

    improved code modularity(made a controller)

---
+++ src/controllers/productController.js
@@ -0,0 +1,78 @@
+const Product = require("../model/product");
+
+//Method to get all posts from db
+async function getAll(req, res) {
+  const page = +req.query.page || 1;
+  const limit = +req.query.limit || 5;
+  const sort = req.query.sort || "";
+  const keyword = req.query.keyword;
+  const query = req.query.query;
+  const pagination = limit != -1;
+  let searchquery;
+  if (keyword) {
+    searchquery = { title: { $regex: keyword, $options: "i" } };
+  }
+
+  const options = {
+    page,
+    limit,
+    pagination,
+    sort,
+  };
+
+  try {
+    const results = await Product.paginate(
+      searchquery || JSON.parse(query || "{}"),
+      options
+    );
+    return res.send({ ...results });
+  } catch (error) {
+    console.error("Error parsing query:", error.message);
+    return res.status(400).json({ error: "Invalid query" });
+  }
+}
+
+//Method to post Product to db
+async function postProduct(req, res) {
+  const {
+    title,
+    moq,
+    image,
+    min_price,
+    max_price,
+    unit,
+    product_certificate,
+    supplier_certificate,
+    country,
+    stock_in_usa,
+    rating,
+  } = req.body;
+
+  // Create a new product object using the provided data
+  const newProduct = new Product({
+    title,
+    moq,
+    image,
+    min_price,
+    max_price,
+    unit,
+    product_certificate,
+    supplier_certificate,
+    country,
+    stock_in_usa,
+    rating,
+  });
+
+  // Save the new product to the database
+  try {
+    const productToSave = await newProduct.save();
+    res.status(200).json(productToSave);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+}
+
+module.exports = {
+  getAll,
+  postProduct,
+};
--- src/routes/product.js
+++ src/routes/product.js
@@ -1,85 +1,15 @@
 const express = require("express");
+const { getAll, postProduct } = require("../controllers/productController");
 const router = express.Router();
-const Product = require("../model/product");
 // const paginatedResults = require("../middleware/pagination");
 
 router.get("/", (req, res) => {
   res.send("Product Api");
 });
+
 //Get all Method
-router.get("/getAllProducts", async (req, res) => {
-  const page = +req.query.page || 1;
-  const limit = +req.query.limit || 5;
-  const sort = req.query.sort || "";
-  const keyword = req.query.keyword;
-  const query = req.query.query;
-  const pagination = limit != -1;
-  let searchquery;
-  if (keyword) {
-    searchquery = { title: { $regex: keyword, $options: "i" } };
-  }
-
-  const options = {
-    page,
-    limit,
-    pagination,
-    sort,
-  };
-
-  try {
-    const results = await Product.paginate(
-      searchquery || JSON.parse(query || "{}"),
-      options
-    );
-    return res.send({ ...results });
-  } catch (error) {
-    console.error("Error parsing query:", error.message);
-    return res.status(400).json({ error: "Invalid query" });
-  }
-});
-
-//Get by ID Method
-router.get("/product/:id", (req, res) => {
-  res.send("Get by ID API");
-});
-
-router.post("/product", async (req, res) => {
-  const {
-    title,
-    moq,
-    image,
-    min_price,
-    max_price,
-    unit,
-    product_certificate,
-    supplier_certificate,
-    country,
-    stock_in_usa,
-    rating,
-  } = req.body;
-
-  // Create a new product object using the provided data
-  const newProduct = new Product({
-    title,
-    moq,
-    image,
-    min_price,
-    max_price,
-    unit,
-    product_certificate,
-    supplier_certificate,
-    country,
-    stock_in_usa,
-    rating,
-  });
-
-  // Save the new product to the database
-  try {
-    const productToSave = await newProduct.save();
-    res.status(200).json(productToSave);
-  } catch (error) {
-    res.status(400).json({ message: error.message });
-  }
-});
+router.get("/getAllProducts", getAll);
+//Post Product Method
+router.post("/product", postProduct);
 
 module.exports = router;
